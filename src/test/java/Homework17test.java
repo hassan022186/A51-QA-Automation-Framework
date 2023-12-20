fdbac020b47a00def1b1a4cc037a06d9a2f5c6d9
hassan <hassan.mahim@testpro.io>
2023-12-20 16:50:14 -0600

    Update README.md

--- src/test/java/Homework17test.java
+++ src/test/java/Homework17test.java
@@ -8,37 +8,37 @@ public class Homework17test extends BaseTest {
     public void addSongToPlaylist() throws InterruptedException {
         String songAddedMessage = "added 1 song into \"Hello.\"";
         //pre-condition
-        navigateToPage()
+        navigateToPage();
         provideEmail("hassan.mahim@testpro.io");
         providePassword("te$t$tudent");
         clickSubmit();
-        Thread.sleep( .2000);
+        Thread.sleep(.2000);
         searchSong("Dark Days");
         viewAllButton();
         firstSong();
         addToButton();
-        selectPlaylist();
+        selectPlaylisT();
         getsongAddedToPlaylistSuccessMsg();
         Assert.assertEquals(getSongAddedToPlaylistsuccessMsg(), songAddedMessage);
 
     }
 
-    public void searchSong(String \"Dark Days"\) throws InterruptedException{
-        WebElement searchfield = driver.findElement(By.cssSelector("div#searchFrom input[type='search']"));
+    public void searchSong(String name) throws InterruptedException{
+        WebElement searchField = driver.findElement(By.cssSelector("div#searchFrom input[type='search']"));
         searchField.sendKeys(name);
-        Thread.sleep( millis: 2000);
+        Thread.sleep( 2000);
     }
 
     public void clickViewAllBtn() throws InterruptedException {
-        webelement viewAll = driver.findeElement(By.xpath( xpathExpression: "//button[@data-test='view-all-songs-btn']"));
+        WebElement viewAll = driver.findeElement(By.xpath( "//button[@data-test='view-all-songs-btn']"));
         ViewAll.click();
-        Thread.sleep( millis: 2000);
+        Thread.sleep( 2000);
     }
 
     public void selectFirstSongResult() throws InterruptedException {
-        webElement firstSong = driver.findElement(By.xpath( xpathExpression: "//section[@id='songResultWrapper']//tr[@class='song-item'][1]"));
+        WebElement firstSong = driver.findElement(By.xpath(  "//section[@id='songResultWrapper']//tr[@class='song-item'][1]"));
         firstSong.click();
-        Thread.sleep( millis: 2000);
+        Thread.sleep(  2000);
     }
 }
 
