d386369d65ffd2d017fb59e001c0e18b3716e7f9
hassan <hassan.mahim@testpro.io>
2023-12-21 14:51:57 -0600

    Merge remote-tracking branch 'origin/homework-17' into homework-17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,15 +1,17 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-
+    protected static WebDriver driver;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
-
+               driver = new ChromeDriver();
     }
 
     public void navigateToPage() {
