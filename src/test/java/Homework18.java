a3d65031a2045d7eacdc5369bb7be0d818fd8ca0
hassan <hassan.mahim@testpro.io>
2024-07-31 23:47:25 -0500

    Merge remote-tracking branch 'origin/homework-17' into homework-17dx

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,18 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework18  extends BaseTest{
-    @Test(priority = 1, description = "play a song and validate that a song is playing")
+public class Homework18  extends BaseTest {
+    @Test
     public void playSong() throws InterruptedException {
-     logIn("hassan.mahim@testpro.io","YOUSSEf2017$");
-      enterAllSongs();
-      selectSong();
-      enterButtonPlaySong();
+        logIn("hassan.mahim@testpro.io", "YOUSSEf2017$");
+        enterAllSongs();
+        selectSong();
+        enterButtonPlaySong();
         Assert.assertTrue(isDisplayedPlayingSong());
-        }
+    }
 }
-
 
 
